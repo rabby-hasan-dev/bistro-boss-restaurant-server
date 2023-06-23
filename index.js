2e2fd389f6c3c264b21491769f2af947d065a6ce
rabby6530 <you@example.com>
2023-06-23 10:58:39 +0600

    added create-payment-intent api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.VITE_payment_secrate_key)
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -136,10 +137,10 @@ async function run() {
 
         })
 
-        app.delete('/menu/:id', verifyJWT, verifyAdmin, async(req,res)=>{
+        app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id;
-            const query={_id: new ObjectId(id)};
-            const result= await menuCollection.deleteOne(query);
+            const query = { _id: new ObjectId(id) };
+            const result = await menuCollection.deleteOne(query);
             res.send(result);
         })
 
@@ -180,6 +181,37 @@ async function run() {
             const result = await cartCollection.deleteOne(query);
             res.send(result)
         })
+
+        // Create payment intent
+        app.post("/create-payment-intent", async (req, res) => {
+
+            const { price } = req.body;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"],
+
+            })
+
+            res.send({
+                clientSecret:paymentIntent.client_secret
+            })
+            // const { items } = req.body;
+
+            // // Create a PaymentIntent with the order amount and currency
+            // const paymentIntent = await stripe.paymentIntents.create({
+            //   amount: calculateOrderAmount(items),
+            //   currency: "usd",
+            //   automatic_payment_methods: {
+            //     enabled: true,
+            //   },
+            // });
+
+            // res.send({
+            //   clientSecret: paymentIntent.client_secret,
+            // });
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
